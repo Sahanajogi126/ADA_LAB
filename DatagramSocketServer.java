3463c902cb3cd9a7f670c12f529019bbb207fed2
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-10-04 08:05:02 +0530

    DatagramSocketServer.java

--- DatagramSocketServer.java
+++ DatagramSocketServer.java
@@ -1,17 +1,24 @@
-import java.net.*;
-import java.io.*;
-
-class DatagramSocketServer {
-    public static void main(String args[]) throws Exception {
-        DatagramSocket serversocket = new DatagramSocket(9999);
-        BufferedReader din = new BufferedReader(new InputStreamReader(System.in));
-        while(true) {
-            byte buf[] = new byte[1024];
-            String str = din.readLine();
-            buf = str.getBytes();
-            InetAddress ip = InetAddress.getByName("127.0.0.1");
-            DatagramPacket dp = new DatagramPacket(buf, str.length(), ip, 1000);
-            serversocket.send(dp);
-        }
-    }
-}
+Server Side Program (DatagramSocketServer.java)	
+import java.io.*; 
+import java.net.*; 
+import java.util.*;
+ public class DatagramSocketServer { 
+public static void main(String args) throws Exception {
+ Scanner in = new Scanner(System.in); DatagramSocket serverSocket = new DatagramSocket(9090); 
+byte receiveData = new byte; byte sendData = new byte; System.out.println("Server Side ***"); 
+    System.out.println("Create the receive datagram packet"); /* Receive packet using server socket */ 
+    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length); serverSocket.receive(receivePacket); /* Extract sender address and port */
+    InetAddress IPAddress = receivePacket.getAddress(); int port = receivePacket.getPort(); 
+    System.out.println("Server is running on port: " + serverSocket.getLocalPort()); 
+    while(true) { /* Convert received message to String */ 
+        String sentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
+        System.out.println("Message received from client side is: " + sentence);
+        System.out.println("Type some message to display at client end"); String serverSentence = in.nextLine(); 
+        sendData = serverSentence.getBytes(); /* Create send packet using client's address and port */ 
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port); 
+        serverSocket.send(sendPacket); /* Prepare for next receive */ 
+        receiveData = new byte; receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        serverSocket.receive(receivePacket); 
+    } 
+   } 
+ }
