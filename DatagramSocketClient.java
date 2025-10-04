afe3cc9e405fdbd45d41db8bbe21be8d74c971cb
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-10-04 15:46:40 +0530

    DatagramSocketClient.java

--- DatagramSocketClient.java
+++ DatagramSocketClient.java
@@ -1,24 +1,22 @@
-Client Side Program (DatagramSocketClient.java)	
-import java.io.*; 
-import java.net.*; 
-import java.util.*; 
-public class DatagramSocketClient { 
-public static void main(String args) throws Exception { 
-DatagramSocket clientSocket = new DatagramSocket(); 
-InetAddress IPAddress = InetAddress.getLocalHost(); 
-byte sendData = new byte; 
-byte receiveData = new byte; 
-Scanner in = new Scanner(System.in);
-System.out.println("Type 'Connected with Client:'"); /* Create the send datagram packet */ 
-while(true) { 
-String clientSentence = in.nextLine(); 
-sendData = clientSentence.getBytes(); 
-DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9090); 
-clientSocket.send(sendPacket); /* Receive the packet using client socket */
-DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-clientSocket.receive(receivePacket); /* Convert the message received into the string */
-String messageReceived = new String(receivePacket.getData(), 0, receivePacket.getLength());
-System.out.println("Message received at client side is: " + messageReceived);
-} 
-} 
+import java.net.*;
+public class DatagramSocketClient {
+    public static void main(String[] args)throws Exception {
+        String line="Connected with Client";
+        DatagramSocket clientSocket=new DatagramSocket();
+        InetAddress IPAdress=InetAddress.getByName("localhost");
+        byte[] sendData=new byte[1024];
+        byte[] receiveData=new byte[1024];
+        sendData=line.getBytes();
+        InetAddress IPAddress = null;
+        DatagramPacket sendPacket=new DatagramPacket(sendData,sendData.length,IPAddress,9000);
+        clientSocket.send(sendPacket);
+        System.out.println("*****Client Display Terminal*****");
+        while(true)
+        {
+            DatagramPacket receivePacket=new DatagramPacket(receiveData,receiveData.length);
+            clientSocket.receive(receivePacket);
+            String messageReceived=new String(receivePacket.getData(),receivePacket.getOffset(),receivePacket.getLength());
+            System.out.println("Message typed at server side is:"+messageReceived);
+        }
+    }
 }
